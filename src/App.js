6a541d3a9a114bdc7bc4e7353af43adb979aaab2
Ginger-Mano <mcasimirian@gmail.com>
2020-12-07 16:37:29 -0500

    para

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Forecast from "./Forecast"
 import './App.css';
+import Paratest from "./paratest"
 
 class App extends Component {
   state = {
@@ -9,26 +10,31 @@ class App extends Component {
 
   componentDidMount() {
     fetch(`https://api.openweathermap.org/data/2.5/find?lat=40.7&lon=-74.0&cnt=10&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
-    .then(function(response) {
-    return response.json();
-    })
-    .then(weatherObj =>
-      // console.log(weatherObj.list)
-      this.setState ({
-        weather: weatherObj.list
+      .then(function (response) {
+        return response.json();
       })
-    )
+      .then(weatherObj =>
+        // console.log(weatherObj.list)
+        this.setState({
+          weather: weatherObj.list
+        })
+      )
   }
   render() {
     console.log(this.state.weather);
-    let triStateWeather = this.state.weather.map(weather => <Forecast weather={weather} key={weather.id}/>)
-  return (
-    <div className="App">
-     <h2>hello</h2>
-     <p>{triStateWeather}</p>
-    </div>
-  )
-}
+    let triStateWeather = this.state.weather.map(weather => <Forecast weather={weather} key={weather.id} />)
+    return (
+      <div className="App">
+        <div >
+          <Paratest />
+        </div>
+        <div className="moving">
+          <h2 className="textmov">Move</h2></div>
+        <h2>hello</h2>
+        <p>{triStateWeather}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/paratest.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react"
+
+export default class Paratest extends Component {
+    render() {
+        return (
+            <div>
+                <div className="test"></div>
+                <p>Candy canes tiramisu cake lollipop pudding icing jujubes. Gummi bears jujubes ice cream chocolate bar gummi bears danish chocolate bar cake. Fruitcake candy canes toffee muffin jelly beans. Tootsie roll ice cream marzipan cotton candy powder danish pudding cupcake.</p>
+                <p>Caramels jujubes sugar plum sweet toffee jelly oat cake. Cookie halvah bear claw bear claw carrot cake soufflé soufflé dragée. Candy tootsie roll bonbon marshmallow chocolate cake bonbon cheesecake tootsie roll caramels. Marzipan jelly-o lemon drops donut.</p>
+                <div class='banner'></div>
+                <p>Candy sesame snaps bear claw cookie. Tart marzipan dessert gummies. Marzipan dragée wafer croissant cake. Biscuit muffin carrot cake.</p>
+                <p>Fruitcake donut icing gummi bears. Donut cake jujubes wafer chocolate bar cotton candy carrot cake. Bear claw tart sweet powder cake. Gummies ice cream dessert cupcake pudding jelly-o.</p>
+                <div class='banner'></div>
+                <p>Marshmallow cupcake marzipan topping tootsie roll jelly-o cake liquorice. Toffee cotton candy danish tiramisu bonbon caramels liquorice halvah. Biscuit donut bear claw chupa chups pudding biscuit powder sweet roll donut. Tart candy canes marzipan.</p>
+                <p>Sugar plum pastry bonbon danish apple pie. Cookie cheesecake candy canes chocolate powder dessert fruitcake jelly. Powder halvah sweet sugar plum lollipop.</p>
+                <p>Powder bonbon chocolate caramels cheesecake soufflé. Cake icing icing ice cream pudding jelly carrot cake ice cream tiramisu. Candy canes wafer marzipan pie chocolate bar dessert jujubes. Biscuit oat cake donut icing brownie.</p>
+                <div class='banner'></div>
+                <div className="test2"></div>
+                <p>Dragée marzipan lollipop sesame snaps chocolate bar toffee jelly. Croissant cupcake sesame snaps tiramisu gummi bears ice cream sugar plum cotton candy. Soufflé cake gummi bears muffin soufflé soufflé marshmallow macaroon. Caramels caramels lemon drops.</p>
+                <p>Croissant oat cake cake. Cheesecake liquorice muffin chocolate cake candy canes powder muffin tiramisu. Candy canes donut jelly beans jelly beans cotton candy jelly beans muffin. Candy apple pie dessert soufflé tiramisu.</p>
+                <div class='banner'></div>
+                <p>Ice cream gingerbread gummi bears pastry dragée muffin. Apple pie cake cake. Chupa chups dessert jelly pastry wafer tootsie roll toffee lemon drops lollipop.</p>
+                <p>Jelly cookie wafer gummi bears caramels toffee. Jelly beans chocolate fruitcake. Lollipop halvah halvah cake tiramisu.</p>
+                <p>Powder halvah sweet marzipan croissant. Tootsie roll carrot cake cheesecake biscuit jelly sugar plum oat cake. Lemon drops bear claw marshmallow chocolate cake biscuit gummies.</p>
+                <p>Tootsie roll carrot cake lollipop pastry fruitcake sesame snaps tootsie roll icing. Soufflé cake croissant candy. Donut wafer candy cookie jujubes marshmallow. Biscuit tart donut gummi bears.</p>
+                <p>Cake donut pudding toffee apple pie sweet roll. Liquorice chocolate bar jujubes jelly-o chupa chups carrot cake dragée macaroon carrot cake. Sesame snaps carrot cake cotton candy danish ice cream caramels bonbon. Apple pie icing brownie dessert marshmallow tootsie roll muffin.</p>
+
+            </div>
+        )
+    }
+}
